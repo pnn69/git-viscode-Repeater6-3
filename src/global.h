e71e30e01782c6cbb75f690b273e5c148ca45166
pnn69 <pnn_69@hotmail.com>
2021-08-18 17:02:59 +0200

    Zerrocross and output corrected

--- src/global.h
+++ src/global.h
@@ -80,11 +80,10 @@
 #define VgainFan 4.556
 #define VgainRJ12 1.68
 #define CompressorOnVoltage 2
-#define LoAnaCount10V 347
-#define HiAnaCount10V 1520
+#define LoAnaCount10V 370
+#define HiAnaCount10V 1550
 #define LoAnaCount12V 373
 #define HiAnaCount12V 3195
-
 
 #define ToutPos 1
 #define TinPos 0
